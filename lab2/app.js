2acb714a2b7c3bf48c1dd36116b592458e2e1514
Rostic <birnaz98@mail.ru>
2020-05-07 22:06:44 +0300

    exercitiul 5

--- lab2/app.js
+++ lab2/app.js
@@ -1,30 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.body.style.backgroundColor = 'green'
     const newDiv = document.createElement('span')
+    newDiv.id="text"
     newDiv.innerText = 'START v3'
     document.body.appendChild(newDiv)
 
  	setTimeout(() => {
      	document.body.style.backgroundColor = 'blue'
      	   let list = document.createElement('ol')
+		    list.id = 'list'
 		    list.setAttribute("style", "list-style: upper-latin;")
 		    list.start = 3;
 		    const numbers = 15
 		    let count = 0
 
 		    setTimeout(() => {
-		    setInterval(() => {
-		        if (count < numbers) {
-		            count++
-		            let li = document.createElement('li')
-		            li.innerText = count
-		            list.appendChild(li)
-		            document.body.appendChild(list)
-		        }
-		    }, 1000)
-
-
-
+			    setInterval(() => {
+			        if (count < numbers) {
+			            count++
+			            let li = document.createElement('li')
+			            li.innerText = count
+			            list.appendChild(li)
+			            document.body.appendChild(list)
+			        }
+			    }, 1000)
+		    
+		    setTimeout(()=>{
+     		    document.getElementById('list').remove()
+        		document.getElementById('text').innerText = "END"
+    		  }, 1000)
 		}, 2000)
     }, 3000);   
 }
